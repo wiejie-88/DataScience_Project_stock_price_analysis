e0c8bec4b2c81b2b13e8ed7f9330e52e51900ba6
wiejie-88 <lwj8768@gmail.com>
2025-02-23 16:33:15 +0800

    PredictTitanicSurvival.py

--- PredictTitanicSurvival.py
+++ PredictTitanicSurvival.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
+
 print("Script started...")  # Debugging Start
 
 # Load the dataset
